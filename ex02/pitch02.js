2edd5f58bb5dce0708f669695524c2e499d78821
Andy Sylvester <andy@andysylvester.com>
2014-09-30 20:15:25 -0700

    Example 2 commit
    Addition of ex02 directory and files for Example 2

---
+++ ex02/pitch02.js
@@ -0,0 +1,19 @@
+var osc;
+var volume;
+
+volume = tsw.gain(0.5);
+
+function turn_on() {
+     osc = tsw.oscillator (600, 'sawtooth');
+     tsw.connect(osc, volume, tsw.speakers);
+     osc.start();
+};
+
+function turn_off() {
+     osc.stop();
+};
+
+function changePitch(newValue)
+{
+    osc.frequency(newValue);
+};
